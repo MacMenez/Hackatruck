0c3a23373d0bcd300eace127cfc4e56d6e827d89
arthur :) <103451087+arthcmm@users.noreply.github.com>
2023-06-13 14:24:03 -0300

    Lumi v2
    Different colors

---
+++ Projeto/Lumi/Lumi/LumiApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumiApp.swift
+//  Lumi
+//
+//  Created by Student03 on 07/06/23.
+//
+
+import SwiftUI
+
+@main
+struct LumiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
