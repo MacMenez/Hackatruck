70b00bb14d3d95271b2bfa7fda73adda5a6c0036
Gabriel Oliveira Peron <114201395+GabrielOlPeron@users.noreply.github.com>
2023-06-13 14:28:32 -0300

    Add files via upload

---
+++ Projeto/UIBackGrounds/UIBackGrounds/UIBackGroundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIBackGroundsApp.swift
+//  UIBackGrounds
+//
+//  Created by Student07 on 13/06/23.
+//
+
+import SwiftUI
+
+@main
+struct UIBackGroundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
