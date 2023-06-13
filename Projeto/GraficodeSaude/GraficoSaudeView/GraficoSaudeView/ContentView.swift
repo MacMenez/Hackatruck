782e7f5700e58187af82f42415c327adcb4661a1
Victor Ferraz <vferraz48@gmail.com>
2023-06-13 15:58:58 -0300

    Tela Gráfico de Saúde

---
+++ Projeto/GraficodeSaude/GraficoSaudeView/GraficoSaudeView/ContentView.swift
@@ -0,0 +1,120 @@
+import SwiftUI
+
+struct BackgroundMain: View {
+    var body: some View {
+                        Rectangle()
+                            .fill(Color(red: 214 / 255, green: 237 / 255, blue: 170 / 255))
+                            .frame(height: 900)
+                        Circle()
+                            .fill(Color(red: 230 / 255, green: 241 / 255, blue: 205 / 255))
+                            .offset(x:-100,y:-300)
+                            .frame(width: 300)
+                        Circle()
+                            .fill(Color(red: 230 / 255, green: 241 / 255, blue: 205 / 255))
+                            .offset(x:150,y:-350)
+                            .frame(width: 200)
+                        Circle()
+                            .fill(Color(red: 230 / 255, green: 241 / 255, blue: 205 / 255))
+                            .offset(x:150,y:299)
+                            .frame(width: 450)
+                        Circle()
+                            .fill(Color(red: 230 / 255, green: 241 / 255, blue: 205 / 255))
+                            .offset(x:-50,y:399)
+                            .frame(width: 300)
+                        Circle()
+                            .fill(Color(red: 230 / 255, green: 241 / 255, blue: 205 / 255))
+                            .offset(x:-140,y:100)
+                            .frame(width: 99)
+                        Circle()
+                            .fill(Color(red: 230 / 255, green: 241 / 255, blue: 205 / 255))
+                            .offset(x:-70,y:70)
+                            .frame(width:150)
+        }
+}
+
+struct ContentView: View {
+    var body: some View {
+        
+        ZStack{
+            BackgroundMain()
+            VStack {
+                Image("YT2ph")
+                    .resizable()
+                    .cornerRadius(25)
+                    .frame(width: 350, height: 200)
+                    .overlay(RoundedRectangle(cornerRadius: 25).stroke(Color.pink))
+                Text("Gráfico de Saúde")
+                    .font(.title)
+                    .fontWeight(.bold)
+                RoundedRectangle(cornerRadius: 25).fill(Color(red: 79    / 255, green: 27 / 255, blue: 134 / 255))
+                    .overlay(VStack{
+                        HStack {
+                            Text("BPM:")
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                            Spacer()
+                        }
+                        HStack {
+                            Text("Pressão:")
+                                .font(.largeTitle)
+                                .fontWeight(.bold)
+                            Spacer()
+                        }
+                        
+                        HStack {
+                            Spacer()
+                            VStack {
+                                Button(action: {
+                                    print("ALERTA")
+                                }) {
+                                    HStack {
+                                        Image(systemName: "exclamationmark.triangle.fill")
+                                            .foregroundColor(.red)
+                                            .font(.system(size: 50))
+                                        Text("ALERTA H:M:S")
+                                            .font(.largeTitle)
+                                            .fontWeight(.bold)
+                                            .foregroundColor(.red)
+                                    }
+                                }
+                                .frame(width: 325.0, height: 80)
+                                .background(RoundedRectangle(cornerRadius: 10).fill(Color(red: 249 / 255, green: 250 / 255, blue: 97 / 255)))
+                                .padding([.top, .trailing])
+                                
+                                Button(action: {
+                                    print("SOCORRO")
+                                }) {
+                                    HStack {
+                                        Image(systemName: "megaphone.fill")
+                                            .foregroundColor(.red)
+                                            .padding(.trailing, 40.0)
+                                            .font(.system(size: 50))
+                                        Text("SOCORRO")
+                                            .font(.largeTitle)
+                                            .fontWeight(.bold)
+                                            .foregroundColor(.red)
+                                            .padding(.trailing, 17.0)
+                                    }
+                                }
+                                .frame(width: 325.0, height: 80)
+                                .background(RoundedRectangle(cornerRadius: 10).fill(Color(red: 249 / 255, green: 250 / 255, blue: 97 / 255)))
+                                .padding([.top, .trailing])
+                            }
+                            Spacer()
+                        }
+                    }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading)
+                    )
+            }
+            .padding()
+            .frame(width: 400, height: 700)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Projeto/GraficodeSaude/GraficoSaudeView/GraficoSaudeView/GraficoSaudeViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraficoSaudeViewApp.swift
+//  GraficoSaudeView
+//
+//  Created by Student on 13/06/23.
+//
+
+import SwiftUI
+
+@main
+struct GraficoSaudeViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
